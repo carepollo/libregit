6863f21f9ee886c2c188d3c5e6ad8b167586581a
ChickenFace <carepollo6000@gmail.com>
2023-06-07 00:27:27 -0500

    testing auth

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
-
-type Maiki struct{}
-
-func (m *Maiki) Lol() {
-	fmt.Println("a")
-}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	var maiki *Maiki
-	maiki.Lol()
+	protectedHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, r.URL.Path+" este es el path xd")
+	}
+
+	authHandler := func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+
+		if !ok || username != "admin" && password != "test" {
+			w.Header().Set("WWW-Authenticate", `Basic Realm="Restricted"`)
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintf(w, "not right credentials")
+			return
+		}
+
+		protectedHandler(w, r)
+	}
+
+	http.HandleFunc("/", authHandler)
+
+	http.ListenAndServe(":5000", nil)
 }
