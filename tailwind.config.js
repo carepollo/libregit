1b0341b6627b7e64c217ac0616c082fce61a2e26
ChickenFace <carepollo6000@gmail.com>
2023-06-13 17:07:05 -0500

    added tailwindcss for site styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js,css,scss}",
+    "./templates/*.{html,js,css,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- web/create_app.go
+++ web/create_app.go
@@ -7,9 +7,14 @@ import (
 
 // initialize web server with correspondant configuration and all application configurations
 func CreateApp() *fiber.App {
+
+	// defining html rendering engine and configuring its behaviour
+	engine := html.New("./templates", ".html")
+
+	// create fiber app and append endpoints
 	app := fiber.New(fiber.Config{
 		CaseSensitive: true,
-		Views:         html.New("./templates", ".html"),
+		Views:         engine,
 	})
 
 	RegisterEndpoints(app)
