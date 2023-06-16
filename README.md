25769db13815cc4fdcf3e56c5b4672794906c7b0
ChickenFace <carepollo6000@gmail.com>
2023-06-16 16:05:25 -0500

    added profile view

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # LibreCode
 
-Your Free software code hosted on a Free software platform.
-
+FOSS compliant community driven highly performant Cloud-Hosted web-based platform and service for Git hosting that respects the privacy of its users and allows them to extend and customize it to meet its needs.
--- web/controllers/render_user.go
+++ web/controllers/render_user.go
@@ -2,21 +2,52 @@ package controllers
 
 import "github.com/gofiber/fiber/v2"
 
+var sampleMD = `# test
+
+**I would like to** _test_ this ~~where~~ I see how powerful is 
+
+<details>
+<summary>Click to expand/collapse</summary>
+
+this markdown compiler
+
+</details>
+
+## how much stuff I can do
+
+| Name    | Age |
+| ------- | --- |
+| Alice   | 25  |
+| Bob     | 30  |
+
+---
+### and how far I can go with this
+
+The quadratic formula is given by:
+
+$$
+x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}
+$$
+
+where a, b, and c are coefficients of a quadratic equation.
+
+
+![something](/hal9000.png)
+
+where I [link](https://google.com) stuff
+`
+
 // handler to render homepage of a user
 func RenderUser(ctx *fiber.Ctx) error {
-	text :=
-		`
-	# this is a test
-                        
-	**lol** I would _love_ to to do that
-	I would not do any of this without you [guys](https://google.com).
-	I would like to share this pic.
+	var homeView string
+	tab := ctx.Query("tab")
+	if tab == "repositories" {
+		homeView = "views/user/list_repos"
+	} else {
+		homeView = "views/user/user"
+	}
 
-	![img test](http://localhost:8080/assets/foss.png)
-
-	and this code:
-	`
-	return ctx.Render("views/user/user", fiber.Map{
-		"sample": text,
+	return ctx.Render(homeView, fiber.Map{
+		"sample": sampleMD,
 	}, "main")
 }
