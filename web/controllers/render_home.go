275f5ce47b1dbed0298b46cac62177379b207df5
ChickenFace <carepollo6000@gmail.com>
2023-06-23 22:53:40 -0500

    logged navbar status rendered

--- web/controllers/render_home.go
+++ web/controllers/render_home.go
@@ -1,10 +1,17 @@
 package controllers
 
 import (
+	"github.com/carepollo/librecode/db"
 	"github.com/gofiber/fiber/v2"
 )
 
 // handler to render the home page, it covers the default landing page and the feed view when logged in
 func RenderHome(ctx *fiber.Ctx) error {
-	return ctx.Render("home", nil, "main")
+	user, err := db.GetUserSession(ctx.IP())
+	contextData := fiber.Map{
+		"IsLogged": err == nil,
+		"User":     user,
+	}
+
+	return ctx.Render("home", contextData, "main")
 }
